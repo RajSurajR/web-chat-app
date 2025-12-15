d9bf5118702c3d0dcb013a9b7eef19e584e03f5a
rajsurajr <rajsurajrajak@gmail.com>
2025-12-15 15:25:17 +0530

    new add on

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,13 +29,13 @@ app.use("/api/auth", authRoutes);
 // ---/api/auth/signup
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV=="production"){
+if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html" ));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
-    // 
+    
 }
 
 server.listen(PORT, ()=>{
